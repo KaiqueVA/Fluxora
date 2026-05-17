d27440d69b3a2255d96382512eab30e57292d372
KaiqueVA <kaique_x_@hotmail.com>
2026-05-17 01:46:20 -0300

    feat: adiciona endpoint para refresh de token e atualiza comando do backend para migrações

--- backend/apps/users/presentation/urls.py
+++ backend/apps/users/presentation/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from apps.users.presentation.views import LoginView, RegisterView
 
@@ -6,4 +7,5 @@ from apps.users.presentation.views import LoginView, RegisterView
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
